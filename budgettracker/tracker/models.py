04805220443b9fe70e73d30a6f1d7cd3a05b18b3
Dragos <larion.dragos96@gmail.com>
2023-05-02 16:53:57 +0300

    Added income, expenses and category models

--- budgettracker/tracker/models.py
+++ budgettracker/tracker/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=200)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.description
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.description
--- budgettracker/tracker/urls.py
+++ budgettracker/tracker/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import CustomLoginView, dashboard, register_request
+from .views import IncomeListView, IncomeCreateView, IncomeUpdateView, IncomeDeleteView
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path("register", register_request, name="register"),
+    path('income/', IncomeListView.as_view(), name='income_list'),
+    path('income/new/', IncomeCreateView.as_view(), name='income_create'),
+    path('income/<int:pk>/edit/', IncomeUpdateView.as_view(), name='income_update'),
+    path('income/<int:pk>/delete/', IncomeDeleteView.as_view(), name='income_delete'),
 ]
